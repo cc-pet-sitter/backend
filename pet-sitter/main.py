7cd726d1b5b48ed5ac64089786278bcb8db562a3
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-11-19 20:18:14 +0900

    Feat: Sets up files and imports for Tortoise ORM

--- pet-sitter/main.py
+++ pet-sitter/main.py
@@ -1,9 +1,14 @@
 #start server from root (backend) folder with "poetry run start"
-from fastapi import FastAPI
-import uvicorn
+from fastapi import FastAPI # type: ignore
+import uvicorn # type: ignore
+from tortoise import Tortoise # type: ignore
 
 app = FastAPI()   
 
 @app.get("/") 
 async def main_route():     
   return "Hello world!"
+
+def start():
+    """Launched with poetry run start at root level"""
+    uvicorn.run("pet-sitter.main:app", port=8000, host="0.0.0.0", reload=True)

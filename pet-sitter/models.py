de63b8ffa302195eaf25f44a02b95ff949848258
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-11-20 16:51:26 +0900

    Feat: Creates tables in DB according to model

--- pet-sitter/models.py
+++ pet-sitter/models.py
@@ -1,6 +1,5 @@
 from tortoise import fields, models # type: ignore
 from enum import Enum
-## DO ENUM STUFF: services, animals, sitter vs owner
 
 class InquiryStatus(Enum):
     REQUESTED = "requested"
@@ -18,24 +17,33 @@ class UserType(Enum):
   OWNER = "owner"
   SITTER = "sitter"
 
+class PetServices(Enum):
+  OWNER_HOUSE = "owner_house"
+  SITTER_HOUSE = "sitter_house"
+  VISIT = "visit"
+
+class AccountLanguage(Enum):
+  ENGLISH = "english"
+  JAPANESE = "japanese"
+
 class Appuser(models.Model):
   id = fields.IntField(primary_key=True)
-  firstname = fields.CharField()
-  lastname = fields.CharField()
-  email = fields.CharField(unique=True)
+  firstname = fields.CharField(max_length=40)
+  lastname = fields.CharField(max_length=40)
+  email = fields.CharField(unique=True, max_length=40)
   account_created = fields.DatetimeField(auto_now_add=True)
   last_updated = fields.DatetimeField(auto_now=True)
   last_login = fields.DatetimeField(null=True)
-  profile_picture_src = fields.CharField(null=True)
-  prefecture = fields.CharField(null=True)
-  city_ward = fields.CharField(null=True)
-  street_address = fields.CharField(null=True)
-  postal_code = fields.CharField(null=True)
-  account_language = fields.CharField(default="English")
+  profile_picture_src = fields.CharField(null=True, max_length=120)
+  prefecture = fields.CharField(null=True, max_length=40)
+  city_ward = fields.CharField(null=True, max_length=40)
+  street_address = fields.CharField(null=True, max_length=40)
+  postal_code = fields.CharField(null=True, max_length=40)
+  account_language = fields.CharEnumField(AccountLanguage, default=AccountLanguage.ENGLISH)
   english_ok = fields.BooleanField(default=False)
   japanese_ok = fields.BooleanField(default=False)
-  owner_id = fields.ForeignKeyField("models.Owner", related_name="id", null=True)
-  sitter_id = fields.ForeignKeyField("models.Sitter", related_name="id", null=True)
+  owner_id = fields.ForeignKeyField("models.Owner", related_name="appusers", null=True)
+  sitter_id = fields.ForeignKeyField("models.Sitter", related_name="appusers", null=True)
 
 class Owner(models.Model):
   id = fields.IntField(primary_key=True)
@@ -59,13 +67,13 @@ class Sitter(models.Model):
   
 class Pet(models.Model):
   id = fields.IntField(primary_key=True)
-  name = fields.CharField()
+  name = fields.CharField(max_length=40)
   type_of_animal = fields.CharEnumField(AnimalType)
-  subtype = fields.CharField(null=True)
+  subtype = fields.CharField(null=True, max_length=40)
   weight = fields.FloatField(null=True)
   birthday = fields.DateField()
-  profile_picture_src = fields.CharField(null=True)
-  owner_id = fields.ForeignKeyField("models.Owner", related_name="id")
+  profile_picture_src = fields.CharField(null=True, max_length=120)
+  owner_id = fields.ForeignKeyField("models.Owner", related_name="pets")
   posted_date = fields.DatetimeField(auto_now_add=True)
   last_updated = fields.DatetimeField(auto_now=True)
   profile_bio = fields.TextField(null=True)
@@ -79,30 +87,30 @@ class Availability(models.Model):
 
 class Review(models.Model):
   id = fields.IntField(primary_key=True)
-  author_appuser_id = fields.ForeignKeyField("models.Appuser", related_name="id")
+  author_appuser_id = fields.ForeignKeyField("models.Appuser", related_name="author_reviews")
   recipient_appuser_type = fields.CharEnumField(UserType)
   comment = fields.TextField(null=True)
   score = fields.IntField(null=True)
-  recipient_appuser_id = fields.ForeignKeyField("models.Appuser", related_name="id")
+  recipient_appuser_id = fields.ForeignKeyField("models.Appuser", related_name="recipient_reviews")
   submission_date = fields.DatetimeField(auto_now_add=True)
 
 class Inquiry(models.Model):
   id = fields.IntField(primary_key=True)
-  owner_appuser_id = fields.ForeignKeyField("models.Appuser", related_name="id")
-  sitter_appuser_id = fields.ForeignKeyField("models.Appuser", related_name="id")
+  owner_appuser_id = fields.ForeignKeyField("models.Appuser", related_name="owner_inquiries")
+  sitter_appuser_id = fields.ForeignKeyField("models.Appuser", related_name="sitter_inquiries")
   inquiry_status = fields.CharEnumField(InquiryStatus, default=InquiryStatus.REQUESTED)
   start_date = fields.DateField()
   end_date = fields.DateField()
-  desired_service = fields.CharField()
-  pet_id_list = fields.CharField()
+  desired_service = fields.CharEnumField(PetServices)
+  pet_id_list = fields.CharField(max_length=80)
   additional_info = fields.TextField(null=True)
   inquiry_submitted = fields.DatetimeField(auto_now_add=True)
   inquiry_finalized = fields.DatetimeField(null=True)
 
 class Message(models.Model):
   id = fields.IntField(primary_key=True)
-  inquiry_id = fields.ForeignKeyField("models.Inquiry", related_name="id")
-  author_appuser_id = fields.ForeignKeyField("models.Appuser", related_name="id")
-  recipient_appuser_id = fields.ForeignKeyField("models.Appuser", related_name="id")
+  inquiry_id = fields.ForeignKeyField("models.Inquiry", related_name="messages")
+  author_appuser_id = fields.ForeignKeyField("models.Appuser", related_name="author_messages")
+  recipient_appuser_id = fields.ForeignKeyField("models.Appuser", related_name="recipient_messages")
   content = fields.TextField()
   time_sent = fields.DatetimeField(auto_now_add=True)

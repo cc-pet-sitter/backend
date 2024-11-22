14fdfc2ec223e6cc71d981310db729f7a5840da3
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-11-22 12:48:57 +0900

    Feat: Adds Base Model classes, Adding Inquiries

--- pet_sitter/main.py
+++ pet_sitter/main.py
@@ -1,11 +1,12 @@
 #start server from root (backend) folder with "poetry run start"
-from fastapi import FastAPI 
+from fastapi import FastAPI, HTTPException
 import uvicorn 
 from tortoise import Tortoise 
 from pydantic import BaseModel 
 from dotenv import load_dotenv
 import os
 import pet_sitter.models as models
+from datetime import datetime
 
 load_dotenv()
 app = FastAPI()   
@@ -15,6 +16,50 @@ class SignUpBody(BaseModel):
 
 class LogInBody(BaseModel):
   email: str
+
+class UpdateAppuserBody(BaseModel):
+  firstname: str | None
+  lastname: str | None
+  email: str | None
+  profile_picture_src: str | None
+  prefecture: str | None
+  city_ward: str | None
+  street_address: str | None
+  postal_code: str | None
+  account_language: str | None
+  english_ok: bool | None
+  japanese_ok: bool | None
+
+class SetSitterBody(BaseModel):
+  average_sitter_rating: float | None
+  profile_bio: str | None
+  bio_picture_src_list: str | None
+  sitter_house_ok: bool | None
+  owner_house_ok: bool | None
+  dogs_ok: bool | None
+  cats_ok: bool | None
+  fish_ok: bool | None
+  birds_ok: bool | None
+  rabbits_ok: bool | None
+  appuser_id: int
+
+class SetOwnerBody(BaseModel):
+  average_sitter_rating: float | None
+  profile_bio: str | None
+  bio_picture_src_list: str | None
+  appuser_id: int
+
+class CreateInquiryBody(BaseModel):
+  owner_appuser_id: int
+  sitter_appuser_id: int
+  start_date: datetime
+  end_date: datetime
+  desired_service: str
+  pet_id_list: str
+  additional_info: str | None
+
+class UpdateInquiryStatusBody(BaseModel):
+  inquiry_status: str
 
 @app.get("/") 
 async def main_route():     
@@ -35,7 +80,7 @@ async def get_detailed_user_info_by_id(id: int):
   return "Here is the appuser you asked for + owner and sitter data!"
 
 @app.post("/appuser-extended/{id}") 
-async def set_user_info(id: int):     
+async def set_user_info(id: int, appuserReqBody: UpdateAppuserBody, sitterReqBody: SetSitterBody, ownerReqBody: SetOwnerBody):     
   return "Here is your updated appuser + owner and sitter data!"
 
 @app.get("/appuser-sitters") 
@@ -51,8 +96,12 @@ async def get_inquiry_by_id(id: int):
   return "Here is the inquiry you asked for!"
 
 @app.post("/inquiry") 
-async def create_inquiry():     
-  return "Here is your new inquiry request!"
+async def create_inquiry(reqBody: CreateInquiryBody):   
+  inquiry = await models.Inquiry.create(**reqBody.dict())  
+  if inquiry:
+    return inquiry
+  else:
+    raise HTTPException(status_code=404, detail=f'Failed to add the inquiry')
 
 @app.patch("/inquiry/{id}") 
 async def update_inquiry_status(id: int):     

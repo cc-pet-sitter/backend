2a8188c37c6b5a0b2ae5a5db7d1fdbf26d6162b0
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-12-04 09:27:46 +0900

    Feat: Adds ability to search by city_ward

--- pet_sitter/main.py
+++ pet_sitter/main.py
@@ -327,13 +327,13 @@ async def get_all_matching_sitters(prefecture: str, city_ward: str | None = None
       if rabbits_ok:
         sitter_search_conditions["rabbits_ok"] = True
 
-      # appuser_search_conditions = {}
-      # if prefecture:
-      #   appuser_search_conditions["prefecture"] = True
-      # if city_ward:
-      #   appuser_search_conditions["city_ward"] = True
+      appuser_search_conditions = {}
+      appuser_search_conditions["appuser__prefecture"] = prefecture
+      
+      if city_ward:
+        appuser_search_conditions["appuser__city_ward"] = city_ward
 
-      matchingSitterArray = await models.Sitter.filter(**sitter_search_conditions).select_related("appuser").filter(appuser__prefecture=prefecture) ## Ex. Can use matchingSitterArray[0].appuser.email to get email from Appuser table for one user
+      matchingSitterArray = await models.Sitter.filter(**sitter_search_conditions).select_related("appuser").filter(**appuser_search_conditions) ## Ex. Can use matchingSitterArray[0].appuser.email to get email from Appuser table for one user
 
       if matchingSitterArray:
         responseArray = []

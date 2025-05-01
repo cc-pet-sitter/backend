8c0874f9f219b3e29734ea70ffd8ac0433ba288b
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-05-01 15:58:23 +0900

    feat: Implements backend support for realtime chat

---
+++ pet_sitter/messaging.py
@@ -0,0 +1,23 @@
+from typing import Dict, List
+from fastapi import WebSocket # type: ignore
+
+class InquiryMessagesManager:
+    def __init__(self):
+      self.active_connections: Dict[int, List[WebSocket]] = {}
+
+    async def connect(self, inquiry_id: int, websocket: WebSocket):
+      await websocket.accept()
+      self.active_connections.setdefault(inquiry_id, []).append(websocket)
+
+    def disconnect(self, inquiry_id: int, websocket: WebSocket):
+      if inquiry_id in self.active_connections:
+        self.active_connections[inquiry_id].remove(websocket)
+
+        if not self.active_connections[inquiry_id]:
+            del self.active_connections[inquiry_id]
+
+    async def broadcast(self, message: dict):
+      for connection in self.active_connections.get(message["inquiry_id"], []):
+          await connection.send_json(message)
+
+inquiry_messages_manager = InquiryMessagesManager()
